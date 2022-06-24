9f4ad7fd67aec3019a55a4a212c450d66567939f
Adhiraj Bhatia <adhirajb1109@gmail.com>
2022-06-24 18:16:26 +0530

    refactor: Refactored the SEO Section

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,47 +8,37 @@ import "@fontsource/jost/700.css";
 import "@fontsource/sen/400.css";
 import "@fontsource/sen/700.css";
 
-import { NextSeo } from "next-seo";
 import Head from "next/head";
-import Script from "next/script";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      <NextSeo
-        title="Adhiraj Bhatia"
-        titleTemplate="Adhiraj Bhatia"
-        defaultTitle="Adhiraj Bhatia"
-        description="A developer who loves to build unique and impactful products."
-        openGraph={{
-          url: "https://adhiraj.site/",
-          title: "Adhiraj Bhatia",
-          description:
-            "A developer who loves to build unique and impactful products.",
-          images: [
-            {
-              url: "/assets/og-image.png",
-            },
-          ],
-        }}
-        twitter={{
-          handle: "@adhirajb1109",
-          site: "@adhirajb1109",
-          cardType: "summary_large_image",
-        }}
-        additionalMetaTags={[
-          {
-            property: "keywords",
-            content:
-              "Developer, Adhiraj, Adhiraj Bhatia, Full Stack Developer, Web Development, Web Developer, Tech Products, Startups, Open Source",
-          },
-        ]}
-      />
       <Head>
+        <meta name="viewport" content="width=device-width" />
         <link rel="icon" type="image/png" href="/assets/avatar.jpg" />
+        <title>Adhiraj Bhatia</title>
+        <meta
+          name="description"
+          content="A developer who loves to build unique and impactful products."
+        />
+        <meta
+          property="keywords"
+          content="Developer, Adhiraj, Adhiraj Bhatia, Full Stack Developer, Web Development, Web Developer, Tech Products, Startups, Open Source"
+        />
+        <meta name="robots" content="index,follow" />
+        <meta name="googlebot" content="index,follow" />
         <meta
           name="google-site-verification"
           content="-F5KEYR7HWa--QIiyrY1KKHdYxMiz5ycQ37WRiMDXns"
+        />
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:site" content="@adhirajb1109" />
+        <meta name="twitter:creator" content="@adhirajb1109" />
+        <meta property="og:image" content="/assets/og-image.png" />
+        <meta property="og:title" content="Adhiraj Bhatia" />
+        <meta
+          property="og:description"
+          content="A developer who loves to build unique and impactful products."
         />
       </Head>
       <Component {...pageProps} />
