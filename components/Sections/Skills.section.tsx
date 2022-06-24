dfc73820d85df57c9b1f5e43cf4b6c9f4912d368
Adhiraj Bhatia <adhirajb1109@gmail.com>
2022-06-24 18:29:46 +0530

    refactor: Removed Unnecessary Libraries

--- components/Sections/Skills.section.tsx
+++ components/Sections/Skills.section.tsx
@@ -3,7 +3,7 @@ import { BsArrowRightShort } from "../Misc/Icons.collection";
 const Skills = () => {
   return (
     <div className="font-sen my-16 px-3 text-white" id="skills">
-      <p className="text-3xl font-bold text-white">Skills & Uses</p>
+      <p className="text-3xl font-bold text-white">Skills</p>
       <div className="text-md custom:text-lg my-8 flex flex-col font-medium md:text-xl">
         <p className="flex flex-row items-center border-b-[0.1px] border-gray-500 py-1 text-slate-300">
           <BsArrowRightShort size="30" />
